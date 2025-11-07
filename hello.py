a606a84ee69c01a6f3e8ae5cfb1ed3702dce6225
theanxiuser <theanxiuser@gmail.com>
2025-11-07 06:46:52 +0545

    first day

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello, world!")
+
+
+
+"""
+print()
+print -> build in function 
+print() -> calling function
+"Hello, World!" -> parameter/argument
+'Hello, World!' -> parameter/argument
+
+It shows "Hello, world!" in the terminal
+"""
