8179be615ccf0467ac41b4601165bd81839f6c4a
theanxiuser <theanxiuser@gmail.com>
2025-11-11 11:27:23 +0545

    day 5

---
+++ greeting.py
@@ -0,0 +1,10 @@
+def greet(name):
+    return f"Namaste, {name}"
+
+
+# print(greet("Gopal"))
+
+# Main function
+if __name__ == "__main__":
+    print("I'm inside main function")
+    print(greet("Gopal"))
